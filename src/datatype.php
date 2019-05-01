6281317f11936dafbf8c3c4970313ecd0f23c9bd
Lycheejam <lycheejamwow@gmail.com>
2019-05-01 13:19:57 +0900

    add data type sample

---
+++ src/datatype.php
@@ -0,0 +1,26 @@
+<?php
+  $lineseparator = "<br>";
+
+  $hoge = "Hello World";
+  var_dump($hoge);
+
+  echo $lineseparator;
+
+  $fuga = true;
+  var_dump($fuga);
+
+  echo $lineseparator;
+
+  $foo = 1234;
+  var_dump($foo);
+
+  echo $lineseparator;
+
+  $bar = 1234.1234;
+  var_dump($bar);
+
+  echo $lineseparator;
+  
+  $foobar = NULL;
+  var_dump($foobar);
+?>
